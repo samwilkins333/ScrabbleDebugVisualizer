e9b53a510a8a9944e76e8ac810c725336c68d4c1
Sam Wilkins <samwilkins333@gmail.com>
2020-08-29 11:57:29 -0700

    bash script organization

--- bash/run.sh
+++ bash/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+
 find . -name "*.java" | xargs javac -g -cp ".:../lib/scrabble-base-jar-with-dependencies.jar"
 java -cp ".:../lib/scrabble-base-jar-with-dependencies.jar" "com/swilkins/ScrabbleVisualizer/executable/$@";
